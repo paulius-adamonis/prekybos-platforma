78101aa6d7f5f39466a9843ac2e9182f80dd06dd
paulius-adamonis <paulius.adamonis@ktu.edu>
2018-12-11 11:53:27 +0200

    Pakeičiau pagrindinio puslapio tekstą bei pervadinau pagrindinio puslapio valdiklį

--- src/Controller/PagrindinioValdiklis.php
+++ src/Controller/PagrindinioValdiklis.php
@@ -5,16 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController extends AbstractController
+class PagrindinioValdiklis extends AbstractController
 {
     /**
      * @Route("/", name="app_main")
      */
     public function index()
     {
+        $message = "Gero naudojimo!";
+
         return $this->render('main/index.html.twig', [
             'title' => 'Pagrindinis',
-            'message' => "Sveiki atvykę į pagrindinį puslapį."
+            'message' => $message
         ]);
     }
 }
